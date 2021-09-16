f720d751454dc33281263a1d2fa6adc4c1aa6dcf
Chancellor Clark <chancellor.clark@bigcommerce.com>
2021-09-16 15:10:24 -0500

    feat(mixins): add stitches padding variants

---
+++ packages/big-design/src/mixins/paddings/stitches/index.ts
@@ -0,0 +1,27 @@
+import { css } from '@bigcommerce/big-design-theme/stitches';
+
+import { tokenVariants } from '../../../utils';
+
+export const cssPaddings = css({
+  variants: {
+    padding: tokenVariants({ token: 'space', css: (value) => ({ padding: value }) }),
+    paddingTop: tokenVariants({ token: 'space', css: (value) => ({ paddingTop: value }) }),
+    paddingRight: tokenVariants({ token: 'space', css: (value) => ({ paddingRight: value }) }),
+    paddingBottom: tokenVariants({ token: 'space', css: (value) => ({ paddingBottom: value }) }),
+    paddingLeft: tokenVariants({ token: 'space', css: (value) => ({ paddingLeft: value }) }),
+    paddingHorizontal: tokenVariants({
+      token: 'space',
+      css: (value) => ({
+        paddingLeft: value,
+        paddingRight: value,
+      }),
+    }),
+    paddingVertical: tokenVariants({
+      token: 'space',
+      css: (value) => ({
+        paddingTop: value,
+        paddingBottom: value,
+      }),
+    }),
+  },
+});
